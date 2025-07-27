6dcba01fe47aab00562fddea64140fc69c038605
Cooltechy <111549972+Cooltechy@users.noreply.github.com>
2025-07-27 15:49:15 +0000

    user model created

---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+const { type } = require('express/lib/response')
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        unique : true,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true,
+        minLength : 8
+    },
+    email : {
+        type : String,
+        unique : true,
+        required : true,
+    }
+})
+
+const userModel = mongoose.model('user',userSchema)
+
+module.exports = userModel
